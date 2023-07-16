f9b7033c3cb5479d0ab353e0c9e1f0e71c7879ea
x-dr <79109673+x-dr@users.noreply.github.com>
2023-07-16 13:31:49 +0800

    Update upload.js

--- asset/js/upload.js
+++ asset/js/upload.js
@@ -213,9 +213,13 @@ function render_uploaded() {
     uploaded_files.forEach(function (x) {
         var resp = x.resp;
         var name = resp.data.url.split('/').pop();
-        const WPRAW = "https://i3.wp.com/telegra.ph"
-        const PhRAW = "https://telegra.ph"
-        var url = resp.data.thumb == null ? BASE_URL + resp.data.url : BASE_URL + resp.data.thumb.url;
+        
+        const WPRAW = "https://i3.wp.com/telegra.ph"  //使用wordpress.com加速
+        const PhRAW = "https://telegra.ph"   //原地址
+        const PROXYURL = ""  //自定义加速域名 默认是使用cloudflare
+        const BASE_PROXYURL = PROXYURL ? PROXYURL : BASE_URL;
+        
+        var url = resp.data.thumb == null ? BASE_PROXYURL + resp.data.url : BASE_PROXYURL + resp.data.thumb.url;
         var wpurl = resp.data.thumb == null ? WPRAW + resp.data.url : WPRAW + resp.data.thumb.url;
         var RAW = resp.data.thumb == null ? PhRAW + resp.data.url : PhRAW + resp.data.thumb.url;
         $('#imagedetail').append(formatHtml({ url: url, code: url, wp: wpurl, raw: RAW }));
