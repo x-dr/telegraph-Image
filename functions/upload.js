36c0e887759f312bd9b1e1eb639434b8bee1bd82
x-dr <xd9722@gmail.com>
2023-08-15 14:20:39 -0400

    fix: 修复视频上传失败问题

--- functions/upload.js
+++ functions/upload.js
@@ -1,16 +1,16 @@
 export async function onRequestPost(context) {
     const { request, env } = context;
     const url = new URL(request.url);
-    const apikey = env.ModerateContentApiKey 
+    const apikey = env.ModerateContentApiKey
     const clientIP = request.headers.get("x-forwarded-for") || request.headers.get("clientIP");
     const Referer = request.headers.get('Referer') || "Referer";
-    
+
     const res_img = await fetch('https://telegra.ph/' + url.pathname + url.search, {
         method: request.method,
         headers: request.headers,
         body: request.body,
     });
-    
+
     const options = {
         timeZone: 'Asia/Shanghai',
         year: 'numeric',
@@ -23,28 +23,30 @@ export async function onRequestPost(context) {
     };
     const timedata = new Date();
     const formattedDate = new Intl.DateTimeFormat('zh-CN', options).format(timedata);
-    
+
     if (!env.IMG) {
         return res_img;
-    }
-    
-    try {
+    } else {
         const newReq = res_img.clone();
         const responseData = await newReq.json();
-        const rating = apikey ? await getRating(apikey, responseData[0].src, url.search) : { rating_index: 0 };
-        await insertImageData(env.IMG, responseData[0].src, Referer, clientIP, rating.rating_index, formattedDate);
-    } catch (e) {
-        console.log(e);
-        await insertImageData(env.IMG, responseData[0].src, Referer, clientIP, 0, formattedDate);
+        try {
+            const rating = apikey ? await getRating(apikey, responseData[0].src) : { rating_index: 0 };
+            await insertImageData(env.IMG, responseData[0].src, Referer, clientIP, rating.rating_index, formattedDate);
+        } catch (e) {
+            console.log(e);
+            await insertImageData(env.IMG, responseData[0].src, Referer, clientIP, 5, formattedDate);
+        }
+
     }
+
 
     return res_img;
 }
 
 
 
-async function getRating(apikey, src, search) {
-    const res = await fetch(`https://api.moderatecontent.com/moderate/?key=${apikey}&url=https://telegra.ph${src}${search}`);
+async function getRating(apikey, src) {
+    const res = await fetch(`https://api.moderatecontent.com/moderate/?key=${apikey}&url=https://telegra.ph${src}`);
     return await res.json();
 }
 
