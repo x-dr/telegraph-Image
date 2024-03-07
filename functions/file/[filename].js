9b799df32194cb6f07f24b58129552c0b6ad6bc9
DipperMorgan <ezioauditorebaobao@gmail.com>
2024-03-07 23:04:57 +0800

    Update [filename].js
    增加限制域名访问

--- functions/file/[filename].js
+++ functions/file/[filename].js
@@ -33,7 +33,7 @@ async function handleRequest(context) {
     const formatter = new Intl.DateTimeFormat('zh-CN', options);
     const formattedDate = formatter.format(timedata);
 
-    const allowedDomains = ["bbs.micromatrix.org"]
+    const allowedDomains = ["bbs.micromatrix.org","micromatrix.org","pan.micromatrix.org"]
     try {
         let refererUrl = new URL(Referer)
         if(!allowedDomains.includes(refererUrl.hostname)){
