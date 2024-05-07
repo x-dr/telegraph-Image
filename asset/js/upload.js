24ed53abbc5daa82ec405c22cc20a96e932fbe61
kateyahu <167114030+kateyahu@users.noreply.github.com>
2024-05-07 12:53:36 +0800

    Update upload.js

--- asset/js/upload.js
+++ asset/js/upload.js
@@ -216,7 +216,7 @@ function render_uploaded() {
         
         const WPRAW = "https://i3.wp.com/telegra.ph"  //使用wordpress.com加速
         const PhRAW = "https://telegra.ph"   //原地址
-        const PROXYURL = "https://picture.dopamine.cloudns.org"  //自定义加速域名 默认是使用cloudflare
+        const PROXYURL = "https://125126590.r.cdn36.com"  //自定义加速域名 默认是使用cloudflare
         const BASE_PROXYURL = PROXYURL ? PROXYURL : BASE_URL;
         
         var url = resp.data.thumb == null ? BASE_PROXYURL + resp.data.url : BASE_PROXYURL + resp.data.thumb.url;
