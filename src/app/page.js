c6aa63521a0904b45f3b9bcd463a44ad5107e79c
x-dr <xd9722@gmail.com>
2024-09-14 13:47:54 +0800

    ⌚️20240914134754

--- src/app/page.js
+++ src/app/page.js
@@ -347,7 +347,7 @@ export default function Home() {
               value={selectedOption} // 将选择框的值绑定到状态中的 selectedOption
               onChange={handleSelectChange} // 当选择框的值发生变化时触发 handleSelectChange 函数
               className="text-lg p-2 border  rounded text-center w-auto sm:w-auto md:w-auto lg:w-auto xl:w-auto  2xl:w-36">
-              <option value="tg">TG （图片大约5小时后失效）</option>
+              <option value="tg">TG （会失效）</option>
               <option value="tgchannel">TG_Channel</option>
               <option value="vviptuangou">vviptuangou</option>
 
