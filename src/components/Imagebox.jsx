cd821adb46857a38cede1dc48fc7ed5f7b1240a1
x-dr <xd9722@gmail.com>
2024-09-15 01:35:20 +0800

    ⌚️20240915013520

--- src/components/Imagebox.jsx
+++ src/components/Imagebox.jsx
@@ -40,7 +40,6 @@ const ImageModal = ({ selectedImageIndex, setSelectedImageIndex, data }) => {
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 modal-overlay">
       <div className=" rounded-lg relative overflow-hidden" ref={modalRef}>
 
-        <div className='bg-white rounded-lg relative overflow-hidden scale-75'>
           <button
             className="absolute bg-red-500 z-10 top-2 right-2 ring-2 text-white hover:text-red-800"
             onClick={handleCloseImage}
@@ -55,7 +54,7 @@ const ImageModal = ({ selectedImageIndex, setSelectedImageIndex, data }) => {
             className="object-cover w-full h-auto rounded-lg"
           />
 
-        </div>
+
         <button
           className="absolute left-2 top-1/2 transform -translate-y-1/2 bg-gray-700 text-white rounded-full w-8 h-8 flex items-center justify-center"
           onClick={handlePrevImage}
