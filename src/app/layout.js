6858a07c13a5046f8057ce744e7431d4b4c5b708
x-dr <xd9722@gmail.com>
2024-09-16 20:32:35 +0800

    add react-photo-view

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import 'react-toastify/dist/ReactToastify.css';
 import 'react-toastify/ReactToastify.min.css';
+import 'react-photo-view/dist/react-photo-view.css';
 import { GoogleAnalytics } from '@next/third-parties/google'
 
 
